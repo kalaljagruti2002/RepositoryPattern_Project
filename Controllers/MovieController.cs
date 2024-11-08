8b51922b684e819e410fc20ce981acf5fbac81a0
Jagruti Kalal <jagruti.kalal1@gmail.com>
2024-11-08 15:42:27 +0600

    Add files via upload

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Interface_Layer.Master_Interface;
+using Model_Layer.Master_Model;
+
+namespace Web_Layer.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        I_Movie imovie;
+        public MovieController(I_Movie init) 
+        {
+            imovie = init;
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+		public ActionResult SaveUpdate(Movie movie)
+		{
+            try
+            {
+                var res = imovie.SaveUpdate(movie);
+                return RedirectToAction("List");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+		}
+        public ActionResult List()
+        {
+            try
+            {
+                var res = imovie.List();
+                return View(res);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            } 
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                var res = imovie.Edit(id);
+                return View("Index", res);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                var res = imovie.Delete(id);
+                return RedirectToAction("List");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+	}
+}
