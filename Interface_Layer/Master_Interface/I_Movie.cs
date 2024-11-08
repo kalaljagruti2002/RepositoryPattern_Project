56aa9c28859042f258abb07cf77990d7ad5d69c2
Jagruti Kalal <jagruti.kalal1@gmail.com>
2024-11-08 15:39:07 +0600

    Add files via upload

---
+++ Interface_Layer/Master_Interface/I_Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model_Layer.Master_Model;
+
+namespace Interface_Layer.Master_Interface
+{
+	public interface I_Movie
+	{
+		string SaveUpdate(Movie _movie);
+		List<Movie> List();
+		Movie Edit(int id);
+		int Delete(int id);
+
+	}
+}
---
+++ Model_Layer/Master_Model/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model_Layer.Master_Model
+{
+	public class Movie
+	{
+		public int Movie_ID { get; set; }
+		public string Movie_Name { get; set; }
+		public string Hero { get; set; }
+		public string Heroin { get; set; }
+		public string Director { get; set; }
+		public string Movie_Ticket { get; set; }
+	}
+}
